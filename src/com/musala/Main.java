41530b6b5f60ccafd0f8fdd4fab7668180a6c9b0
Miroslav Mironov <miroslav.mironov@musala.com>
2019-03-07 09:11:19 +0100

    Implemented a call.

--- src/com/musala/Main.java
+++ src/com/musala/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello, this is not an SVN repo");
     }
+
+    public void call() {
+        
+    }
 }
